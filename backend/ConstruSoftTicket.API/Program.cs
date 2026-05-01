5bbf78e071ab1900245fb79adfcdd439e06b27d6
Ryan Vargas <brayanvargassedano@gmail.com>
2026-04-30 19:37:27 -0500

    feat: configuración de API

--- backend/ConstruSoftTicket.API/Program.cs
+++ backend/ConstruSoftTicket.API/Program.cs
@@ -1,5 +1,8 @@
 using ConstruSoftTicket.Application.Interfaces;
 using ConstruSoftTicket.Application.Services;
+using ConstruSoftTicket.Infrastructure.Data;
+using ConstruSoftTicket.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<ITicketRepository, TicketRepository>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql("Host=localhost;Port=5432;Database=ConstruSoftTicketDB;Username=postgres;Password=vargas01"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
